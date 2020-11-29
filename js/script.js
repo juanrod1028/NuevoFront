1fa8cabdd3c1d4a0f67196b7abfa2044bfa29374
JuanRod <juanrodriguez1028@gmail.com>
2020-11-28 21:34:49 -0500

    3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var direccion = "/cgi-bin/BlogBack";
+var direccion = "/cgi-bin/NuevoBack";
 function registrarUsuario(obj){
     $.ajax({
         method: 'POST',
@@ -9,7 +9,7 @@ function registrarUsuario(obj){
             response=JSON.parse(rta);
             if(response.tipo==="OK"){
                 alert("Mensaje: "+response.mensaje)
-                $(location).attr('href','/BlogFront/BlogFront.html')
+                $(location).attr('href','/NuevoFront/BlogFront.html')
             }
             else{
                 alert("Error: "+response.mensaje)
@@ -32,7 +32,7 @@ function loginUsuario(obj){
             response=JSON.parse(rta);
             if(response.tipo==="OK"){
                 alert("Mensaje: "+response.mensaje)
-                $(location).attr('href','/BlogFront/BlogFront.html')
+                $(location).attr('href','/NuevoFront/BlogFront.html')
             }
             else{
                 alert("Error: "+response.mensaje)
