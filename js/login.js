3b4115a5a38cf36e65c5d0ad76e74a17f6bfefe4
Nicolas <luis.arboleda@correo.usa.edu.co>
2021-04-19 12:13:25 -0500

    login arreglado 1

--- js/login.js
+++ js/login.js
@@ -1,17 +1,13 @@
 //Recoge los datos de Login.html y los manda a login.py
 $("#Ingresar").click(function(e){
     e.preventDefault();
-    var username = $("#usr").val();
+    var email = $("#email").val();
     var password = $("#pass").val();
-    if(validarEmail(email)){
-        var obj = { 
-            nombre: username,
-            contraseña: password
-        }
-        loginUsuario(obj);
-    }else{
-        alert("Formato de correo inválido") 
+    var obj = { 
+        correo: email,
+        contraseña: password
     }
+    loginUsuario(obj);
 });
 
 
