c39b3525a30a69861c21cdf842ab058637a95082
Nicolas <luis.arboleda@correo.usa.edu.co>
2021-04-19 16:48:49 -0500

    agregar producto 3

---
+++ js/producto.js
@@ -0,0 +1,49 @@
+$("#btnAgregarProduc").click(function(e){
+    e.preventDefault();
+    var precio=$("#prec").val();
+    var id=$("#id").val();
+    var name=$("#name").val();
+    var imagen=$("#image").val();
+    var categoria=$("#categ").val();
+    var obj={
+        precio: precio,
+        id: id,
+        nombre: name,
+        imagen: imagen,
+        categoria: categoria
+    }  
+    crearProtuducto(obj)
+}
+);
+
+$("#btnActualizar").click(function(e){
+    e.preventDefault();
+    var idCategoria=$("#idCategoria").val();
+    var idProducto=$("#idProducto").val();
+    var nombre=$("#nombre").val();
+    var unidad=$("#unidad").val();
+    var precio=$("#precio").val();
+    var imagen=$("#imagen").val();
+    var obj={
+        idCategoria: idCategoria,
+        idProducto: idProducto,
+        nombre: nombre,
+        unidad: unidad,
+        precio: precio,
+        imagen: imagen
+    };
+    actualizarProducto(obj)
+}
+);
+
+$("#btnEliminar").click(function(e){
+    e.preventDefault();
+    var idProducto=$("#idProducto").val();
+    var nombre=$("#nombre").val();
+    var imagen=$("#imagen").val();
+    var obj={
+            idProducto: idProducto
+    };
+    eliminarProducto(obj)
+}
+);
