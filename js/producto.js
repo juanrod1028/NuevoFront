b6feacb630478875f7a171c0b3fad185e95d5e04
Nicolas <luis.arboleda@correo.usa.edu.co>
2021-04-19 18:26:29 -0500

    arreglar actualizar y eliminar prodcuto 2

--- js/producto.js
+++ js/producto.js
@@ -18,19 +18,17 @@ $("#btnAgregarProduc").click(function(e){
 
 $("#btnActualizarProduc").click(function(e){
     e.preventDefault();
-    var idCategoria=$("#idCategoria").val();
-    var idProducto=$("#idProducto").val();
-    var nombre=$("#nombre").val();
-    var unidad=$("#unidad").val();
-    var precio=$("#precio").val();
-    var imagen=$("#imagen").val();
+    var precio=$("#prec").val();
+    var id=$("#id").val();
+    var name=$("#name").val();
+    var imagen=$("#image").val();
+    var categoria=$("#categ").val();
     var obj={
-        idCategoria: idCategoria,
-        idProducto: idProducto,
-        nombre: nombre,
-        unidad: unidad,
         precio: precio,
-        imagen: imagen
+        id: id,
+        nombre: name,
+        imagen: imagen,
+        categoria: categoria
     };
     actualizarProducto(obj)
 }
